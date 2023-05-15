8c6c14809ee80679c45e3ac7aa4a27c60c34238e
Rui Nascimento <57668890+sruinascimento@users.noreply.github.com>
2023-05-15 09:37:36 -0300

    commit inicial

---
+++ src/test/java/br/com/apinaruto/ApiNarutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apinaruto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNarutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
